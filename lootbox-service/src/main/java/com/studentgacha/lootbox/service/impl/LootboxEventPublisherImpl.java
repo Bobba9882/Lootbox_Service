0f21f55a1f472b0a698feb90b4c5bf81e3b6a7ab
Jesse Leppens <jesseleppens@hotmail.com>
2025-02-27 14:55:19 +0100

    fixed bug with queue name

--- lootbox-service/src/main/java/com/studentgacha/lootbox/service/impl/LootboxEventPublisherImpl.java
+++ lootbox-service/src/main/java/com/studentgacha/lootbox/service/impl/LootboxEventPublisherImpl.java
@@ -3,12 +3,17 @@ package com.studentgacha.lootbox.service.impl;
 import com.studentgacha.lootbox.DTOs.LootboxOpenedEvent;
 import com.studentgacha.lootbox.service.LootboxEventPublisher;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LootboxEventPublisherImpl implements LootboxEventPublisher {
 
     private final RabbitTemplate rabbitTemplate;
+
+    @Value("${rabbitmq.lootbox.queue:lootbox.events}") // Default to "lootbox.events" if property is missing
+    private String lootboxQueue;
+
 
     public LootboxEventPublisherImpl(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
@@ -18,8 +23,8 @@ public class LootboxEventPublisherImpl implements LootboxEventPublisher {
     @Override
     public void publishLootboxOpened(Long userId, Long itemId) {
         LootboxOpenedEvent event = new LootboxOpenedEvent(userId, itemId);
-        String lootboxQueue = "${rabbitmq.lootbox.queue}";
+        System.out.println("Sending event to RabbitMQ: " + event);
         rabbitTemplate.convertAndSend(lootboxQueue, event);
-        System.out.println("Published event: " + event);
+        System.out.println("Event sent successfully!");
     }
 }
