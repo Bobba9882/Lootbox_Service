643ee175361cc8f5f2e84839c10b69d712bf3ab3
Jesse Leppens <jesseleppens@hotmail.com>
2025-02-27 11:07:58 +0100

    Added gitignore and set-up project

---
+++ lootbox-service/src/main/java/com/studentgacha/lootbox/LootboxServiceApplication.java
@@ -0,0 +1,13 @@
+package com.studentgacha.lootbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LootboxServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LootboxServiceApplication.class, args);
+	}
+
+}
---
+++ lootbox-service/src/test/java/com/studentgacha/lootbox/LootboxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentgacha.lootbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LootboxServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
